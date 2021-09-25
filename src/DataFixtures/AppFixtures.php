949c48bf44cedf17f5e99053d7bc13dceb984a4d
alexlemia13 <alexlemia13@outlook.fr>
2021-09-25 14:28:00 +0200

    créé fixtures, navbar et front article_index

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -7,33 +7,51 @@ use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private $passwordHasher;
+
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher) {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+
     public function load(ObjectManager $manager)
     {
         $faker = Faker\Factory::create('fr_FR');
 
+        $users = [];
+        
 
         for($i = 0; $i < 10; $i++) {
-            $user = new User;
+            $user = new User();
             $user->setEmail($faker->email);
-            $user->setPassword('password');
-
+            $user->setPassword($this->passwordHasher->hashPassword($user, 'password'));
+            $users[] = $user;
             $manager->persist($user);
         }
-
-
-        for($i = 0; $i < 20; $i++) {
-            $article = new Article();
-            $article->setCreatedAt(new \DateTime($faker->date()));
-            $article->setTitle(implode(' ', $faker->words(rand(1, 6))));
-            $article->setContent(implode(' ', $faker->sentences(rand(5, 30))));
-            $article->setPhoto('https://picsum.photos/200/300?random='.$i);
-            $article->setAuthor();
-            $manager->persist($article);
-        }
         
+        $admin = new User();
+        $admin->setEmail('admin@admin.admin');
+        $admin->setPassword($this->passwordHasher->hashPassword($admin, 'password'));
+        $admin->setRoles(['ROLE_ADMIN']);
+        $users[] = $admin;
+        $manager->persist($admin);
+        
+        
+        for($i = 0; $i < 20; $i++) {
+                $article = new Article();
+                $article->setCreatedAt(new \DateTime($faker->date()));
+                $article->setTitle(implode(' ', $faker->words(rand(1, 6))));
+                $article->setContent(implode(' ', $faker->sentences(rand(5, 30))));
+                $article->setPhoto('https://picsum.photos/200/300?random='.$i);
+                $article->setAuthor($users[rand(0, count($users)-1)]);
+                $manager->persist($article);
+            }
+            
         $manager->flush();
     }
 }
